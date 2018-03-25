02ec1a4245cc5ea2411fa04bbedd024c27260c2e
Jtekt <github.jtekt@gmail.com>
2018-03-25 19:42:15 -0400

    Added handler for RetroPi data deduplication.

--- pi-backup/backup.sh
+++ pi-backup/backup.sh
@@ -2,7 +2,7 @@
 src="/"
 dest=$1
 host=$HOSTNAME
-romsdir="/home/pi/RetroPie/roms"
+romsdir="/home/pi/RetroPie/roms/"
 exclude_list="/home/pi/scripts/pi-backup/exclude.txt"
 #Verify destination provided
 if [ -z "$dest" ];then
@@ -11,17 +11,18 @@ if [ -z "$dest" ];then
 fi
 #Verify remote connection
 ssh -q -o BatchMode=yes -o ConnectTimeout=10 -i /home/pi/.ssh/id_rsa pi@$dest exit
-if [ "$?" -ne "0" ]
+if [ "$?" -ne "0" ];
 then
        echo "Remote destination is not reachable"
        exit 1
 fi
-sudo rsync -achv -e "ssh -i /home/pi/.ssh/id_rsa" --info=progress2 --timeout=12000 --delete --chmod=+r --exclude-from=$exclude_list $src pi@$dest:$host/data
-if [ -d "$romsdir"]; then
-	sudo rsync -achv -e "ssh -i /home/pi/.ssh/id_rsa" --info=progress2 --timeout=12000 --delete --chmod=+r $romsdir pi@$dest:/util/roms
+#sudo rsync -achv -e "ssh -i /home/pi/.ssh/id_rsa" --info=progress2 --timeout=12000 --delete --chmod=+r --exclude-from=$exclude_list $src pi@$dest:$host/data
+if [ -d "$romsdir" ];
+then
+	sudo rsync -achv -e "ssh -i /home/pi/.ssh/id_rsa" --info=progress2 --timeout=12000 --delete --chmod=+r $romsdir pi@$dest:util/roms
 fi
 # Check status
-if [ "$?" -eq "0" ]
+if [ "$?" -eq "0" ];
 then
         echo "Backup complete."
 else
