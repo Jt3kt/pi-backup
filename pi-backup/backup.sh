bd8c4a03c17bfab462a31963940e3cea611719af
Jtekt <github.jtekt@gmail.com>
2018-03-25 19:26:18 -0400

    Added handler for RetroPi roms data deduplication.

--- pi-backup/backup.sh
+++ pi-backup/backup.sh
@@ -2,6 +2,7 @@
 src="/"
 dest=$1
 host=$HOSTNAME
+romsdir="/home/pi/RetroPie/roms"
 exclude_list="/home/pi/scripts/pi-backup/exclude.txt"
 #Verify destination provided
 if [ -z "$dest" ];then
@@ -16,6 +17,9 @@ then
        exit 1
 fi
 sudo rsync -achv -e "ssh -i /home/pi/.ssh/id_rsa" --info=progress2 --timeout=12000 --delete --chmod=+r --exclude-from=$exclude_list $src pi@$dest:$host/data
+if [ -d "$romsdir"]; then
+	sudo rsync -achv -e "ssh -i /home/pi/.ssh/id_rsa" --info=progress2 --timeout=12000 --delete --chmod=+r $romsdir pi@$dest:/util/roms
+fi
 # Check status
 if [ "$?" -eq "0" ]
 then
